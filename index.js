589df9b198da962be2097911bfd35aff283bd0a4
cbarnes0 <carsonbarnes98@gmail.com>
2023-03-12 13:54:16 -0400

    removed comments

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
 
-
-// TODO: Create an array of questions for user input
 const questions = [
     {
     type: 'input',
@@ -54,7 +52,6 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
     if (err) {
@@ -64,7 +61,6 @@ function writeToFile(fileName, data) {
     }
   });}
 
-// TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions)
   .then((data) => {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
     if (license === 'MIT') {
       return '![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)';
@@ -14,8 +13,6 @@ function renderLicenseBadge(license) {
     }
   }
   
-  // TODO: Create a function that returns the license link
-  // If there is no license, return an empty string
   function renderLicenseLink(license) {
     if (license === 'MIT') {
       return 'https://opensource.org/licenses/MIT';
@@ -30,8 +27,6 @@ function renderLicenseBadge(license) {
     }
   }
   
-  // TODO: Create a function that returns the license section of README
-  // If there is no license, return an empty string
   function renderLicenseSection(license) {
     if (license !== "None") {
       return `## License
@@ -43,7 +38,6 @@ function renderLicenseBadge(license) {
     return "";
   }
   
-  // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
       const { title, description, installation, usage, contributing, tests, license, github, email } = data;
     
